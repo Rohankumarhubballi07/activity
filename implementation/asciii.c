5dd4dbcaaa2b7698b3012291f630568280629839
Rohankumar Shekhar Hubballi <82749120+Rohankumarhubballi07@users.noreply.github.com>
2022-05-05 12:07:29 +0530

    Create asciii.c

---
+++ implementation/asciii.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int mychar (char str[]);
+
+ int main()
+ {
+   char str[100];         
+   int sum=0;
+   printf("\nEnter any Name of a string:\n ");
+   scanf("%s",str);
+   printf("\nASCII values of each characters of given string:\n ");
+   sum = mychar(str);
+   printf("\nsum is %d\n",sum);
+   return 0;
+ }
+ int mychar (char str[100])      // function 
+ {
+   int i=0,sum=0;
+    while(str[i]>0)
+    {
+     printf("%d",str[i++]);
+      sum = sum+(int)str[i];   
+   }
+   return sum;
+}
+
+
+
+  
+
+  
